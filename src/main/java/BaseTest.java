9f8d528e2c799dc69c5de278f5e1f55cd80b18c9
Murali Raghu <55809672+muraliraghu7@users.noreply.github.com>
2022-10-04 12:13:02 +0530

    Update BaseTest.java
     base class for browser initialization

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+
+    static WebDriver driver;
+
+    public void setupBrowser(String browserName) {
+        switch (browserName) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","src\\main\\resources\\browserDrivers\\chromedriver.exe");
+                driver = new ChromeDriver();
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver","src\\main\\resources\\browserDrivers\\geckodriver.exe");
+                driver = new FirefoxDriver();
+
+            driver.manage().window().maximize();
+        }
+
+    }
 }
